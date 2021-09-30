d9c8a547dde09e4303e013beca4e4d51d9582c92
EC2 Default User <ec2-user@ip-172-31-91-29.ec2.internal>
2021-09-30 05:40:01 +0000

    updated Dev Docker file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload -- Changed by parag.
+          Edit <code>src/App.js</code> and save to reload --  30-Sep-2021
         </p>
         <a
           className="App-link"
